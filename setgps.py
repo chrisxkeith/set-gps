e6e749eadb4a2ddbdf752ebd5e36f8eef618bb82
CK <chris.keith@gmail.com>
2025-07-04 13:35:16 -0700

    Handles error conditions

--- setgps.py
+++ setgps.py
@@ -3,13 +3,30 @@ import sys
 import os
 
 def main():
-    # CSV_FILE = "/c/Users/chris/Downloads//List of Stinehour sites - Sites.csv"
     CSV_FILE = "C:\\Users\\chris\\Downloads\\List of Stinehour sites - Sites.csv"
-    # PICTURE_DIRECTORY = ""
+    PICTURE_DIRECTORY = "C:\\Users\\chris\\Pictures\\Stinehour Sites\\"
     with open(CSV_FILE, mode='r', newline='') as file:
         reader = csv.DictReader(file)
         for row in reader:
-            print(row["Filename"])
+            if not row["Filename"]:
+                print("Skipping row with no filename.")
+                continue
+            full_path = os.path.join(PICTURE_DIRECTORY, row["Filename"])
+            latitude = row["Latitude"]
+            longitude = row["Longitude"]
+            if not latitude and not longitude:
+                print(f"Skipping {full_path} due to missing GPS data.")
+                continue
+            # Case doesn't matter in Windows
+            for ext in ['.jpg', '.jpeg', '.png']:
+                found = False
+                if os.path.exists(full_path + ext):
+                    found = True
+                    command = f'exiftool -GPSLatitude*="{latitude}" -GPSLongitude*="{longitude}" "{full_path + ext}"'
+                    print(command)
+                    os.system(command)
+            if not found:
+                print(f"File not found: {full_path}")
 
 if __name__ == "__main__":
     main()
