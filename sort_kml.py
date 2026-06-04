69596e0412a326de0f96b0b11e2a4d5323be6d7a
Chris Keith <chris.keith@gmail.com>
2026-06-04 09:46:22 -0700

    Add URL checking

--- sort_kml.py
+++ sort_kml.py
@@ -3,11 +3,42 @@ import csv
 import datetime
 import sys
 import re
+import urllib.request
+from urllib.error import HTTPError, URLError
 
 # To make it easier to pretty print the XML, use lxml instead of ElementTree.
 # sudo apt install python3-pip
 # pip install lxml
 from lxml import etree # type: ignore
+
+def verify_urls(urls):
+    """Open each URL and verify it does not return HTTP 404."""
+    results = []
+    for url in urls:
+        print(".", end="", flush=True)
+        try:
+            request = urllib.request.Request(url, method='HEAD')
+            with urllib.request.urlopen(request, timeout=10) as response:
+                status = response.getcode()
+        except HTTPError as error:
+            if error.code == 405:
+                # Some servers do not allow HEAD, retry with GET.
+                request = urllib.request.Request(url, method='GET')
+                with urllib.request.urlopen(request, timeout=10) as response:
+                    status = response.getcode()
+            else:
+                status = error.code
+        except URLError as error:
+            results.append((url, False, str(error.reason)))
+            continue
+
+        if status == 404:
+            results.append((url, False, status))
+        else:
+            results.append((url, True, status))
+    print(flush=True)
+    return results
+
 
 def log(message):
     script_name = sys.argv[0]
@@ -32,6 +63,7 @@ class Sorter:
         kml = tree.getroot()
         document = kml[0]
         dict = {}
+        URLS = []
         knownCount = 0
         unknownCount = 0
         for placemark in document:
@@ -41,6 +73,10 @@ class Sorter:
                 self.add_unknown(placemark[0])
             else:
                 knownCount += 1
+            url = re.search(r'\bhttps?://\S+', placemark[0].text)
+            if url:
+                url = url.group(0)[0:url.group(0).find(']')]
+                URLS.append(url)
         log("# of known points: " + str(knownCount))
         # log("# of unknown points: " + str(unknownCount))
         sortedPoints = sorted(dict.items(), key=lambda item: item[0])
@@ -48,6 +84,10 @@ class Sorter:
             document.remove(placemark)
         for placemark in sortedPoints:
             document.append(placemark[1])
+        results = verify_urls(URLS)
+        for url, is_valid, status in results:
+            if not is_valid:
+                log("Invalid URL: " + url + " (status: " + str(status) + ")")
 
     def write_kml_file(self, fn, tree):
         with open(fn, 'w') as kml_file:
@@ -69,5 +109,46 @@ class Sorter:
         self.write_kml_file(fn, tree)
         # self.write_unknowns()
 
+    def test(self):
+        URLS = ["https://www.google.com", 
+                "https://www.nonexistentwebsite123456789.com",
+                "https://photos.app.goo.gl/u563BjuqZ1Hra5zw"]
+        results = verify_urls(URLS)
+        for url, is_valid, status in results:
+            if is_valid:
+                log("Valid URL: " + url + " (status: " + str(status) + ")")
+            else:
+                log("Invalid URL: " + url + " (status: " + str(status) + ")")
+
+    def check_unknowns(self):
+        unknowns = [
+            "https://photos.app.goo.gl/u563BjuqZ1Hra5zw",
+            "https://photos.app.goo.gl/YM7FXvxXEuGNhu1j",
+            "https://photos.app.goo.gl/eeYpQWW9HZoh6Jkj",
+            "https://photos.app.goo.gl/uPF88FFr4bcMt6V8",
+            "https://photos.app.goo.gl/jeqvCekZqta7EWPq",
+            "https://photos.app.goo.gl/r35zq1nDjpz8x7c9",
+            "https://photos.app.goo.gl/XyvmRfyxmn4qkspaA",
+            "https://photos.app.goo.gl/iv9DjPrdG2ihHraV",
+            "https://photos.app.goo.gl/TY2oKPohRbWeW2xY",
+            "https://photos.app.goo.gl/xt1RuE3gCSgPh4wE",
+            "https://photos.app.goo.gl/vtp69oYJvY3FGNp6",
+            "https://photos.app.goo.gl/bm5KJqwh2SgoRuKX",
+            "https://photos.app.goo.gl/GdNQAzPqfYUarj5k",
+            "https://photos.app.goo.gl/owpwpJ1qczUEMJxb",
+            "https://photos.app.goo.gl/6Bny3T1dEAwnhXmC",
+            "https://photos.app.goo.gl/yXLwB4eW2V4HD2sT6",
+            "https://photos.app.goo.gl/9jZm2oqPtJyQibp2",
+            "https://photos.app.goo.gl/RkkGYDNsTQKe7K6m",
+            "https://photos.app.goo.gl/WbitRXJAfLtLkdkF"]
+
+        results = verify_urls(unknowns)
+        for url, is_valid, status in results:
+            if is_valid:
+                log("Valid URL: " + url + " (status: " + str(status) + ")")
+            else:
+                log("Invalid URL: " + url + " (status: " + str(status) + ")")
+
 if '__main__' == __name__:
     Sorter().main()
+    Sorter().check_unknowns()
